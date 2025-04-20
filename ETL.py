e0589ed60fac94241cc44bae733e6869da44772f
Unnikrishnan-9747 <unnikurupar@gmail.com>
2025-04-20 18:59:53 +0100

    Update ETL.py
    updated the imports

--- ETL.py
+++ ETL.py
@@ -1,1 +1,24 @@
-
+from dagster import job, op, DagsterInstance, execute_job, reconstructable, Output, Out
+import pandas as pd
+import pymongo
+from pymongo import MongoClient
+import psycopg2
+from psycopg2.extras import execute_batch
+import json
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+import plotly.express as px
+import plotly.io as pio
+import dash
+from dash import dcc, html
+import dash_bootstrap_components as dbc
+from fpdf import FPDF
+import os
+import sys
+from pathlib import Path
+import logging
+import traceback
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+import seaborn as sns
+from datetime import datetime
